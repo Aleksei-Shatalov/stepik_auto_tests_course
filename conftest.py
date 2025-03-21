c203f7d1180517f2ce023a0510bc6f039fd7c84e
Aleksei-Shatalov <shatalov.aleksei@gmail.com>
2025-03-21 15:15:19 +0800

    Add module 3 test files and test_items.py

---
+++ conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose user language: ru, eng, etc")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    print("\nstart chrome browser for test..")
+    browser.maximize_window()
+
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
